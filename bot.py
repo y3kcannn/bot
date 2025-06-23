30777863c07a1292bed768a594e512324f6088fb
y3kcannn <astroslater3922@gmail.com>
2025-06-23 18:54:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import requests
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,7 +16,6 @@ async def on_ready():
 async def ban(ctx, username: str):
     url = "https://midnightponywka.com/loader/banned.php"
     data = {'username': username}
-
     try:
         response = requests.post(url, data=data)
         if response.status_code == 200 and "Success" in response.text:
@@ -29,7 +29,6 @@ async def ban(ctx, username: str):
 async def unban(ctx, username: str):
     url = "https://midnightponywka.com/loader/unban.php"
     data = {'username': username}
-
     try:
         response = requests.post(url, data=data)
         if response.status_code == 200 and "Success" in response.text:
@@ -39,5 +38,5 @@ async def unban(ctx, username: str):
     except Exception as e:
         await ctx.send(f"⚠️ Hata oluştu: {str(e)}")
 
-# BURAYA BOT TOKENİNİ YAPIŞTIR
-bot.run("MTM4NjU3MDQ3MDk1NDEwNzAxMQ.GVFy_w.h9uy9Bw3c4CIKq6_dsLpYvdjOL5MBiAZA0q8gI")
+# Bot token'ı Railway'de environment olarak ayarlanmalı
+bot.run(os.getenv("DISCORD_TOKEN"))
